bee80ad8e2d1ab2c2307017c8ff27674d7596e8b
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-07-18 18:09:09 +0400

    Initial commit

--- src/main/java/main/repo/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package main.repo;
-
-import main.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepo extends JpaRepository<Admin, Long> {
-}
--- src/main/java/main/repo/WarehouseRepo.java
+++
@@ -1,9 +0,0 @@
-package main.repo;
-
-import main.model.Warehouse;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WarehouseRepo extends JpaRepository<Warehouse, Long> {
-}
